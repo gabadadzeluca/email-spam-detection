e5ba603da6b19670d1065ff4decd137a078615db
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-02-17 23:04:25 +0400

    add return statement and return features + vector; also change min_df to 5

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from explore_data import read_data
 
-texts = read_data()
+texts = read_data()[0] # Get training data only for feature extraction
 
 def extract_features(texts):
   """
@@ -13,17 +13,19 @@ def extract_features(texts):
     # use idf weighting
   """
 
-  tfidf = TfidfVectorizer(min_df=1, max_df=0.8, sublinear_tf=True, use_idf=True)
+  tfidf = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)
   features = tfidf.fit_transform([text[1] for text in texts])
   df = pd.DataFrame(
     features.toarray(), 
     columns=tfidf.get_feature_names_out()
   )
+
+  features = tfidf.fit_transform(texts)
   print("Extracted features shape:", df.shape)
 
   # Look at index-th email's non-zero features
   doc_index = 0
   non_zero = df.iloc[doc_index][df.iloc[doc_index] > 0]
   print(non_zero)
-
-extract_features(texts)
+  
+  return features, tfidf
