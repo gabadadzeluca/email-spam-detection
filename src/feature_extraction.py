35132bd795995875fe0950122e773e59e25b2036
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-02-17 23:16:05 +0400

    remove line that used letters instead of words

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,6 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-from explore_data import read_data
+from src.explore_data import read_data
 
 texts = read_data()[0] # Get training data only for feature extraction
 
@@ -14,7 +14,7 @@ def extract_features(texts):
   """
 
   tfidf = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)
-  features = tfidf.fit_transform([text[1] for text in texts])
+  features = tfidf.fit_transform(texts)
   df = pd.DataFrame(
     features.toarray(), 
     columns=tfidf.get_feature_names_out()
